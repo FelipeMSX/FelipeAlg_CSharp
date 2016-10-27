33693889510905a147658ff877cd8e1e81ca8207
felipemsx <felipe_link64@hotmail.com>
2016-10-27 17:58:11 -0300

    Adicionado métodos na pasta sort

---
+++ Algorithms/Algorithms_Test/sort/QuickSort.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms_Test.sort
+{
+	[TestClass]
+	public class QuickSort
+	{
+		[TestMethod]
+		[TestCategory("Sort")]
+		public void TestQuickSort()
+		{
+		}
+	}
+}
