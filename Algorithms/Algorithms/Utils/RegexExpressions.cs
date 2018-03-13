7857c7b2a82275e1708c415dabaf61287547fcc9
Felipe Morais Aragão <Felipe_link64@hotmail.com>
2018-03-13 07:58:28 -0300

    Adicionados testes para RegexExpressions

--- Algorithms/Algorithms/Utils/RegexExpressions.cs
+++ Algorithms/Algorithms/Utils/RegexExpressions.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace Algorithms.Utils
 {
-	public static class RegexExpressions
+	public class RegexExpressions
 	{
-		public const String AddressIP= @"^\d{3}\.\d{3}\.\d{3}\.\d{1,3}$";
-		public const String CPF = @"^\d{3}\.\d{3}\.\d{3}\-\d{2}$";
-		public const String NUCNPJ = @"^\d{2}\.\d{3}\.\d{3}\W\d{4}\-\d{2}$";
-	}
+        public const String AddressIP = @"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$";
+        public const String CPF = @"^\d{3}\.\d{3}\.\d{3}\-\d{2}$";
+        public const String CNPJ = @"^\d{2}\.\d{3}\.\d{3}\W\d{4}\-\d{2}$";
+    }
 }
---
+++ Algorithms/Algorithms_Test/Utils/RegexExpressionsTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Algorithms.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms_Test.Utils
+{
+    [TestClass]
+    public class RegexExpressionsTests
+    {
+
+        #region CPF Valid Inputs
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void CPF_ValidPattern_ValidCPF()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch("048.111.333-78", RegexExpressions.CPF);
+            //Assert
+            Assert.IsTrue(result, "The input is valid because the CPF pattern doesn't match!");
+        }
+
+        #endregion
+
+        #region CPF Invalid Inputs
+
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void CPF_MissingACharacter_InvalidCPF()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch("048.111.333-0", RegexExpressions.CPF);
+            //Assert
+            Assert.IsFalse(result, "The input is invalid because the CPF pattern doesn't match!");
+        }
+
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void CPF_MissingAllCharacteres_InvalidCPF()
+        {
+            //Arrange
+            RegexExpressions regex = new RegexExpressions();
+            //Act
+            bool result = Regex.IsMatch("", RegexExpressions.CPF);
+            //Assert
+            Assert.IsFalse(result, "The input is invalid because the CPF pattern doesn't match!");
+        }
+
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions"), ExpectedException(typeof(ArgumentNullException))]
+        public void CPF_NullInput_InvalidCPF()
+        {
+            //Arrange
+            RegexExpressions regex = new RegexExpressions();
+            //Act
+            bool result = Regex.IsMatch(null, RegexExpressions.CPF);
+            //Assert
+            Assert.Inconclusive("An exception was expected!");
+        }
+
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void CPF_OnlyNumbers_InvalidCPF()
+        {
+            //Arrange
+            RegexExpressions regex = new RegexExpressions();
+            //Act
+            bool result = Regex.IsMatch("04811133340", RegexExpressions.CPF);
+            //Assert
+            Assert.IsFalse(result, "The input is invalid because the CPF pattern doesn't match!");
+        }
+        #endregion
+
+    }
+}
