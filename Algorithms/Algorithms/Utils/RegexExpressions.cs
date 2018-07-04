57ff19ebab51f61c19f32c50829a665d56635b12
Felipe Morais Aragão <Felipe_link64@hotmail.com>
2018-07-04 01:22:45 -0300

    Testes finalizados na classe RegexExpression

--- Algorithms/Algorithms/Utils/RegexExpressions.cs
+++ Algorithms/Algorithms/Utils/RegexExpressions.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Algorithms.Utils
 {
-	public class RegexExpressions
+    /// <summary>
+    /// Objeto da classe é realizar validações na camada de exibição.
+    /// </summary>
+    /// <author>Felipe Morais</author>
+    /// <email>felipemsx18@gmail.com</email>
+    /// <typeparam name="E"></typeparam>
+    public static class RegexExpressions
 	{
         public const String AddressIP = @"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$";
         public const String CPF = @"^\d{3}\.\d{3}\.\d{3}\-\d{2}$";
--- Algorithms/Algorithms_Test/Utils/RegexExpressionsTests.cs
+++ Algorithms/Algorithms_Test/Utils/RegexExpressionsTests.cs
@@ -9,7 +9,7 @@ namespace Algorithms_Test.Utils
     public class RegexExpressionsTests
     {
 
-        #region CPF Valid Inputs
+        #region CPF - Valid Inputs
         /// <summary>
         /// Técnica: Particionamento de Equivalência.
         /// </summary>
@@ -20,12 +20,12 @@ namespace Algorithms_Test.Utils
             //Act
             bool result = Regex.IsMatch("048.111.333-78", RegexExpressions.CPF);
             //Assert
-            Assert.IsTrue(result, "The input is valid because the CPF pattern doesn't match!");
+            Assert.IsTrue(result, "The input is valid because the CPF has a match!");
         }
 
         #endregion
 
-        #region CPF Invalid Inputs
+        #region CPF - Invalid Inputs
 
         /// <summary>
         /// Técnica: Particionamento de Equivalência.
@@ -47,7 +47,6 @@ namespace Algorithms_Test.Utils
         public void CPF_MissingAllCharacteres_InvalidCPF()
         {
             //Arrange
-            RegexExpressions regex = new RegexExpressions();
             //Act
             bool result = Regex.IsMatch("", RegexExpressions.CPF);
             //Assert
@@ -61,7 +60,6 @@ namespace Algorithms_Test.Utils
         public void CPF_NullInput_InvalidCPF()
         {
             //Arrange
-            RegexExpressions regex = new RegexExpressions();
             //Act
             bool result = Regex.IsMatch(null, RegexExpressions.CPF);
             //Assert
@@ -75,11 +73,181 @@ namespace Algorithms_Test.Utils
         public void CPF_OnlyNumbers_InvalidCPF()
         {
             //Arrange
-            RegexExpressions regex = new RegexExpressions();
             //Act
             bool result = Regex.IsMatch("04811133340", RegexExpressions.CPF);
             //Assert
             Assert.IsFalse(result, "The input is invalid because the CPF pattern doesn't match!");
+        }
+        #endregion
+
+
+        #region CNPJ - Valid Inputs
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void CNPJ_ValidPattern_ValidCPNJ()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch("86.218.925/0001-29", RegexExpressions.CNPJ);
+            //Assert
+            Assert.IsTrue(result, "The input is valid because the CNPJ has a match!");
+        }
+
+        #endregion
+
+        #region CNPJ - Invalid Inputs
+
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void CNPJ_MissingADigit_InvalidCNPJ()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch("86.218.925/001-29", RegexExpressions.CNPJ);
+            //Assert
+            Assert.IsFalse(result, "CNPJ is invalid because one character is missing!");
+        }
+
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void CNPJ_WithCharacter_InvalidCNPJ()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch("86.218.A25/001-29", RegexExpressions.CNPJ);
+            //Assert
+            Assert.IsFalse(result, "CNPJ must contain only numbers with mask!");
+        }
+
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void CNPJ_MissingAllCharacteres_InvalidCNPJ()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch("", RegexExpressions.CNPJ);
+            //Assert
+            Assert.IsFalse(result, "The CNPJ is invalid because it is empty!");
+        }
+
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions"), ExpectedException(typeof(ArgumentNullException))]
+        public void CPNJ_NullInput_InvalidCNPJ()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch(null, RegexExpressions.CNPJ);
+            //Assert
+            Assert.Inconclusive("It's not acceptable null value!");
+        }
+
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void CNPJ_OnlyNumbers_InvalidCNPJ()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch("877317600003157", RegexExpressions.CNPJ);
+            //Assert
+            Assert.IsFalse(result, "CNPJ is only valid with mask!");
+        }
+
+        #endregion
+
+
+        #region IP - Valid Inputs
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void IP_ValidPattern_ValidIP()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch("172.168.218.116", RegexExpressions.AddressIP);
+            //Assert
+            Assert.IsTrue(result, "The input is valid because the IP has a match!");
+        }
+
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void IP_ValidPatternWithOneSingleDigit_ValidIP()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch("1.2.3.4", RegexExpressions.AddressIP);
+            //Assert
+            Assert.IsTrue(result, "The input is valid because the IP has a match!");
+        }
+
+        #endregion
+
+
+        #region IP - Invalid Inputs
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void IP_PatternWithCharacterNotValid_InvalidIP()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch("1a2.168.2a8.116", RegexExpressions.AddressIP);
+            //Assert
+            Assert.IsFalse(result, "The input is valid because the IP has a match!");
+        }
+
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void IP_MissingAllCharacteres_InvalidIP()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch("", RegexExpressions.AddressIP);
+            //Assert
+            Assert.IsFalse(result, "The IP is invalid because it is empty!");
+        }
+
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions"), ExpectedException(typeof(ArgumentNullException))]
+        public void IP_NullInput_InvalidIP()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch(null, RegexExpressions.AddressIP);
+            //Assert
+            Assert.Inconclusive("It's not acceptable null value!");
+        }
+
+        /// <summary>
+        /// Técnica: Particionamento de Equivalência.
+        /// </summary>
+        [TestMethod, TestCategory("RegexExpressions")]
+        public void IP_OnlyNumbers_InvalidIP()
+        {
+            //Arrange
+            //Act
+            bool result = Regex.IsMatch("172168218116", RegexExpressions.AddressIP);
+            //Assert
+            Assert.IsFalse(result, "IP is only valid with mask!");
         }
         #endregion
 
